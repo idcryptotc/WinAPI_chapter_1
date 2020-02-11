675d67ccf4a7397776e887b7fcb8dbdeaa47853a
Fanateek <idcryptotc@yandex.ru>
2020-02-12 00:26:57 +0300

    Собрано окно фуллскрин поверх всех, выход по ЛКМ.

--- WinAPI_chapter_1/WinAPI_chapter_1/main_file.cpp
+++ WinAPI_chapter_1/WinAPI_chapter_1/main_file.cpp
@@ -1,12 +1,99 @@
-#include <iostream>
-#include <conio.h>
+#include <Windows.h>
+#include <tchar.h>
 
-using namespace std;
+LRESULT __stdcall WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int main()
+TCHAR WinName[] = _T("Main");
+
+int __stdcall _tWinMain(HINSTANCE This, HINSTANCE Prev, LPTSTR cmd, int mode)
 {
-	setlocale(LC_ALL, "Russian");
-	cout << "Hello, World!" << endl;
-	_getch();
+	HWND hWnd;
+	MSG msg;
+	WNDCLASS wcMain;
+
+	wcMain.hInstance = This;
+	wcMain.lpszClassName = WinName;
+	wcMain.lpfnWndProc = WndProc;
+	wcMain.style = CS_HREDRAW | CS_VREDRAW;
+	wcMain.hIcon = LoadIcon(NULL, IDI_ERROR);
+	wcMain.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcMain.lpszMenuName = NULL;
+	wcMain.cbClsExtra = 0;
+	wcMain.cbWndExtra = 0;
+	wcMain.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+	if (!RegisterClass(&wcMain))
+	{
+		return 0;
+	}
+
+	HDC hScreenDC;
+	hScreenDC = GetDC(0);
+	int screen_x = GetDeviceCaps(hScreenDC, HORZRES);
+	int screen_y = GetDeviceCaps(hScreenDC, VERTRES);
+
+	hWnd = CreateWindow
+	(
+		WinName,
+		_T("Первый блок заданий"),
+		WS_EX_TOPMOST | WS_POPUP | WS_VISIBLE,
+		0,
+		0,
+		screen_x,
+		screen_y,
+		HWND_DESKTOP,
+		NULL,
+		This,
+		NULL
+	);
+	ShowWindow(hWnd, mode);
+
+	while (GetMessage(&msg, NULL,0,0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	DeleteDC(hScreenDC);
+	return 0;
+}
+
+LRESULT __stdcall WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE:
+		{
+			break;
+		}
+	case WM_SIZE:
+		{
+			break;
+		}
+	case WM_MDIMAXIMIZE:
+		{
+			break;
+		}
+	case WM_LBUTTONUP:
+		{
+			PostQuitMessage(0);
+			break;
+		}
+	case WM_ACTIVATEAPP:
+		{
+			SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 1);
+			break;
+		}
+	case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			break;
+		}
+	default:
+		{
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+
 	return 0;
 }
